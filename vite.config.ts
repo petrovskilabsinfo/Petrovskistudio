f66ca2cdba3a5c37f3cd10d0c3eedeca9e46e050
petrovskilabsinfo <petrovskilabsinfo@gmail.com>
2025-12-13 12:22:48 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +9,17 @@ export default defineConfig({
     port: 5175,
     open: true,
   },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  base: './', // This is important for Vercel
 });
