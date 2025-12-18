f012f2834b68d9fe49dee4fa6728a2df93f153f1
petrovskilabsinfo <petrovskilabsinfo@gmail.com>
2025-12-18 17:25:06 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(() => ({
-  base: '/',
+export default defineConfig({
   plugins: [react()],
   server: {
     port: 5175,
     open: true,
   },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-        },
-      },
-    },
-  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-}));
+});
